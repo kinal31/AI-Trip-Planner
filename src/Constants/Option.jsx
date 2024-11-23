6bb3992f01576efd46b368eea2a7a36f069fcd39
ravis-teamopine <ravis@teamopine.com>
2024-11-23 13:14:04 +0530

    23/11

---
+++ src/Constants/Option.jsx
@@ -0,0 +1,38 @@
+
+export const SelectTravelesList =[
+    {
+        "id": 1,
+        "title": "Just Me",
+        "desc": "A sole traveles in exploration",
+        "icon" :'✈️',
+        "people":"1"
+    },
+    {
+        "id": 2,
+        "title": "A Couple",
+        "desc": "Two traveles in tandem",
+        "icon" :'🥂',
+        "people":"1"
+    },
+    {
+        "id": 3,
+        "title": "Family",
+        "desc": "A group of fun loving adv",
+        "icon" :'🏡',
+        "people":"3 to 5 people"
+    },
+    {
+        "id": 4,
+        "title": "Friends",
+        "desc": "A bunch of thrill-seekes",
+        "icon" :'⛵',
+        "people":"5 to 10 people"
+    },
+    {
+        "id": 4,
+        "title": "Friends",
+        "desc": "A bunch of thrill-seekes",
+        "icon" :'⛵',
+        "people":"5 to 10 people"
+    },
+]
--- src/Pages/CreateTrip.jsx
+++ src/Pages/CreateTrip.jsx
@@ -1,6 +1,8 @@
+import { Input } from '@/components/ui/input';
 import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 // import { useJsApiLoader } from "@react-google-maps/api";
+
 
 const CreateTrip = () => {
   const [place, setPlace] = useState();
@@ -19,18 +21,28 @@ const CreateTrip = () => {
       <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
 
-      <div className='mt-20'>
+      <div className='mt-20 flex flex-col gap-10'>
+
         <div>
           <h2 className='text-xl my-3 font-medium '>What is destination of choice?</h2>
-         
-          
+        
           <GooglePlacesAutocomplete apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}  
             selectProps={{
             place,
-            onChange: (value) => setPlace(value)
+            onChange: (value) => {setPlace(value); console.log(value); },
             }}
           />
         </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip?</h2>
+          <Input type='number' placeholder='Ex:3' />
+        </div>
+
+      </div>
+
+      <div>
+        <h2 className='text-xl my-3 font-medium'>What is your budget for this trip?</h2>
       </div>
     </div>
   )
