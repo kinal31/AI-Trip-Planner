c98fe038d51d116a92b50dca0310806462179db6
Kinal <kinalkotheeya31@gmail.com>
2024-11-24 17:12:30 +0530

    24/11 - second change

--- src/Pages/CreateTrip.jsx
+++ src/Pages/CreateTrip.jsx
@@ -3,11 +3,14 @@ import React, { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 // import { useJsApiLoader } from "@react-google-maps/api";
 import axios from "axios";
+import { SelectBudgetOptions, SelectTravelesList } from '../Constants/Option';
+import { Button } from '@/components/ui/button';
 
 
 const CreateTrip = () => {
   const [place, setPlace] = useState();
-  
+  const [formData, setFormData] = useState([])
+
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(-1);
@@ -17,7 +20,7 @@ const CreateTrip = () => {
   const API_KEY = "AlzaSyLPhfgMJoYGtE6B22EbGbExDjtdIP1cNFj";
 
   const fetchSuggestions = async (value) => {
-    if (value.length > 1) {
+    if (value.length > 0) {
       try {
         const response = await axios.get(API_URL, {
           params: {
@@ -36,10 +39,14 @@ const CreateTrip = () => {
     }
   };
 
-  const handleChange = (e) => {
+  const handleChange = (name,value) => {
     const value = e.target.value;
     setInputValue(value);
     fetchSuggestions(value);
+    setFormData({
+      ...formData,
+      [name]: value
+    })
   };
 
   const handleSelect = (suggestion) => {
@@ -71,7 +78,7 @@ const CreateTrip = () => {
 
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌴</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
 
       <div className='mt-20 flex flex-col gap-10'>
@@ -140,11 +147,44 @@ const CreateTrip = () => {
           <Input type='number' placeholder='Ex:3' />
         </div>
 
+        <div>
+          <h2 className='text-xl my-3 font-medium'>What is Your budget?</h2>
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectBudgetOptions.map((item, index) => {
+              return (
+                <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                  <h2 className='text-4xl'>{item.icon}</h2>
+                  <h2 className='font-bold text-lg'>{item.title}</h2>
+                  <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+                </div>
+              );
+            })}
+          </div>
+
+        </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>Who do you plan on traveling with on your next adventure?</h2>
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectTravelesList.map((item, index) => {
+              return (
+                <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                  <h2 className='text-4xl'>{item.icon}</h2>
+                  <h2 className='font-bold text-lg'>{item.title}</h2>
+                  <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+                </div>
+              );
+            })}
+          </div>
+
+        </div>
+
       </div>
 
-      <div>
-        <h2 className='text-xl my-3 font-medium'>What is your budget for this trip?</h2>
+      <div className='my-10 flex justify-end'>
+        <Button>Generate Trip</Button>
       </div>
+
     </div>
   )
 }
