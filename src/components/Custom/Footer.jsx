157164731d3d09ad6f2daa4d1a61357e79e4c9f3
Kinal <128071723+kinal31@users.noreply.github.com>
2024-12-10 17:54:12 +0530

    10/12

--- src/components/Custom/Footer.jsx
+++ src/components/Custom/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
     return (
         <>
             <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 sticky bottom-0 w-full">
-                <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 text-center">
+                <div className="w-full max-w-screen-xl mx-auto text-center">
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    <span className="block text-sm text-gray-500 sm:text-center mb-3 dark:text-gray-400">
                         © 2024 Kinal Kotheeya All Rights Reserved.
                     </span>
                 </div>
